da46ec2861d4a9870f4f26f3e2d8c2b6a2a8d9c3
Emily <emilyjharrison1@gmail.com>
2023-01-31 14:28:37 +0100

    Added Search Engine Functionality

--- src/app.js
+++ src/app.js
@@ -44,3 +44,37 @@ if (minutes <= 9) {
 let year = now.getFullYear();
 
 showDate();
+
+//Add search Engine Functionality
+function showCurrentTemperature(response) {
+  let currentCity = response.data.city;
+  let currentCountry = response.data.country;
+  let currentTemperature = Math.round(response.data.temperature.current);
+  let humidity = response.data.temperature.humidity;
+  let realFeel = Math.round(response.data.temperature.feels_like);
+  let windSpeed = Math.round(response.data.wind.speed);
+  let weatherDesc = response.data.condition.description;
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = `${currentCity}, ${currentCountry}`;
+  let mainTemp = document.querySelector(".current-temperature");
+  mainTemp.innerHTML = `${currentTemperature}`;
+  let humid = document.querySelector(".humidity");
+  humid.innerHTML = `Humidity: ${humidity}%`;
+  let feelsLike = document.querySelector(".feels-like");
+  feelsLike.innerHTML = `Real Feel: ${realFeel}°`;
+  let wind = document.querySelector(".wind");
+  wind.innerHTML = `Wind Speed: ${windSpeed} km/h`;
+  let weatherDescription = document.querySelector(".weather-desc");
+  weatherDescription.innerHTML = `${weatherDesc}`;
+}
+
+function searchCity(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#search-input");
+  let searchedCity = cityInput.value;
+  let apiKey = "776b6d9305acf4056ofe6t078ed04517";
+  let weatherApiUrl = `https://api.shecodes.io/weather/v1/current?query=${searchedCity}&key=${apiKey}&units=metric`;
+  axios.get(weatherApiUrl).then(showCurrentTemperature);
+}
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", searchCity);
