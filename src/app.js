007598b474461d4c21da2a17f2d97c17bdea7a13
Emily <emilyjharrison1@gmail.com>
2023-01-31 13:35:19 +0100

    Added current location data functionality in JS

--- src/app.js
+++ src/app.js
@@ -1,1 +1,46 @@
+// Display Correct Date
+function showDate() {
+  let currentDate = document.querySelector(".date");
+  let currentTime = document.querySelector(".time");
+  currentDate.innerHTML = `${day}, ${month} ${date} ${year}`;
+  currentTime.innerHTML = `${hour}:${minutes}`;
+}
+let now = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
+let day = days[now.getDay()];
+let date = now.getDate();
+let month = months[now.getMonth()];
+let hour = now.getHours();
+if (hour <= 9) {
+  hour = `0${hour}`;
+}
+let minutes = now.getMinutes();
+if (minutes <= 9) {
+  minutes = `0${minutes}`;
+}
+let year = now.getFullYear();
+
+showDate();
